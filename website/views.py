ab214fa6dd49f87fdbbe0304b7f4bf09a3417f89
EricGPTI <ericgpti@gmail.com>
2021-04-02 22:19:46 -0300

    Site funcionando

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(requests):
+    return render(requests, 'index.html')
+
+
+def gallery(requests):
+    return render(requests, 'galeria.html')
